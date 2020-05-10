56f71cfe2831be94716d5c71f663016d879cd1fc
DESKTOP-8700K\bkote <bkoteski88@gmail.com>
2020-05-10 18:34:44 +1000

    add Quote model

---
+++ WorldCities/Data/Models/Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace WorldCities.Data.Models
+{
+    public class Quote
+    {
+        public string Symbol { get; set; }
+        public string Date { get; set; }
+        public decimal PxOpen { get; set; }
+        public decimal PxClose { get; set; }
+    }
+}
